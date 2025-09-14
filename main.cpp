0666d7370677441d1a725522aac99918572fcd91
Roman Byrak <arkanquer@gmail.com>
2025-09-14 11:18:39 +0300

    add readPointsFromFile for CreatePoint initialization - reads number of iterations, start point and base points - returns initialized CreatePoint object - handles file open error

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <random>
 #include <fstream>
+#include <sstream>
+
 
 using namespace std;
 
@@ -31,11 +33,29 @@ public:
   const vector<pair<double, double>> getBasePoints() const {
     return pointsOfPlane;
   }
-
-  bool empty() const {
-    return pointsOfPlane.empty();
-  }
 };
+
+CreatePoint readPointsFromFile(const string& filename) {
+  ifstream file(filename);
+  if (!file.is_open()) {
+    cerr << "The opening file error: " << filename << endl;
+  }
+
+  int n;
+  file >> n;
+
+  double x0, y0;
+  file >> x0 >> y0;
+
+  vector<pair<double, double>> bases;
+  double x_b, y_b;
+  while (file >> x_b >> y_b) {
+    bases.push_back({x_b, y_b});
+  }
+
+  file.close();
+  return CreatePoint({x0, y0}, bases);
+}
 
 int main() {
   return 0;
