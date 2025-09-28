1099083ef4cb9d62dffa0b900c1cceda715ffd0f
Roman Byrak <arkanquer@gmail.com>
2025-09-28 23:44:46 +0300

    Fix file input checking in readPointsFromFile

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <iostream>
 #include <vector>
 #include <stdexcept>
@@ -10,60 +9,57 @@ using namespace std;
 
 class CreatePoint {
 private:
-  pair<double, double> currentPoint;
-  vector<pair<double, double>> pointsOfPlane;
-
+    pair<double, double> currentPoint;
+    vector<pair<double, double>> pointsOfPlane;
 public:
-  CreatePoint(pair<double, double> start, vector<pair<double, double>> points)
-      : currentPoint(start), pointsOfPlane(points) {
-    try {
-      if (pointsOfPlane.empty()) {
-        throw invalid_argument("the list is empty");
-      }
-    } catch (const invalid_argument &e) {
-      cerr << "error in CreatePoint constructor:  " << e.what() << endl;
+    CreatePoint(pair<double, double> start, vector<pair<double, double>> points)
+        : currentPoint(start), pointsOfPlane(points) {
+        try {
+            if (pointsOfPlane.empty()) {
+                throw invalid_argument("the list is empty");
+            }
+        } catch (const invalid_argument &e) {
+            cerr << "error in CreatePoint constructor:  " << e.what() << endl;
+        }
     }
-  }
-
-  pair<double, double> getCurrent() const {
-    return currentPoint;
-  }
-
-  const vector<pair<double, double>> getBasePoints() const {
-    return pointsOfPlane;
-  }
+    pair<double, double> getCurrent() const {
+        return currentPoint;
+    }
+    const vector<pair<double, double>> getBasePoints() const {
+        return pointsOfPlane;
+    }
 };
-
 CreatePoint readPointsFromFile(const string& filename) {
-  ifstream file(filename);
-  if (!file.is_open()) {
-    cerr << "The opening file error: " << filename << endl;
-  }
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "The opening file error: " << filename << endl;
+    }
 
-  int n;
-  if(!(file >> n)){
-    cerr << "The input error" << endl;
-  }
+    int n;
+    file >> n;
+    if(!(file >> n)){
+        cerr << "The input error" << endl;
+    }
 
-  double x0, y0;
-  if(!(file >> x0 >> y0)){
-    cerr << "The input error" << endl;
-  }
+    double x0, y0;
+    file >> x0 >> y0;
+    if(!(file >> x0 >> y0)){
+        cerr << "The input error" << endl;
+    }
 
-  vector<pair<double, double>> bases;
-  double x_b, y_b;
-  while (file >> x_b >> y_b) {
-    bases.push_back({x_b, y_b});
-  }
+    vector<pair<double, double>> bases;
+    double x_b, y_b;
+    while (file >> x_b >> y_b) {
+        bases.push_back({x_b, y_b});
+    }
 
-  if (bases.empty()) {
+    if (bases.empty()) {
         cerr << "Error: base points are not found." << endl;
     }
 
-  file.close();
-  return CreatePoint({x0, y0}, bases);
+    file.close();
+    return CreatePoint({x0, y0}, bases);
 }
-
 int main() {
-  return 0;
+    return 0;
 }
